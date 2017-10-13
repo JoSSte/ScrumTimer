3693d6530bea254870eb92b882062c3e2f80b194
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2017-10-13 07:26:45 +0200

    added shuffle, start, stop buttons

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -12,55 +12,81 @@ export class TimerComponent implements OnInit {
   private future: Date;
   private futureString: string;
   private diff: number;
+  private currentDiff: number;
+  private currentElapsed: number = 0;
   private $counter: Observable<number>;
+
+  private totalTime: number = 10;
+
   private subscription: Subscription;
   private message: string;
-  participantList: Participant[];
+  private participantList: Participant[];
+  private currentParticipant = 0;
 
   constructor(public participantService: ParticipantService, elm: ElementRef) {
-    this.futureString = elm.nativeElement.getAttribute('inputDate');
+    //this.futureString = elm.nativeElement.getAttribute('inputDate');
     //console.log(this.futureString);
     //this.futureString = 'January 1, 2018 12:00:00';
   }
-
-  dhms(t) {
-    var days, hours, minutes, seconds;
-    days = Math.floor(t / 86400);
-    t -= days * 86400;
-    hours = Math.floor(t / 3600) % 24;
-    t -= hours * 3600;
-    minutes = Math.floor(t / 60) % 60;
-    t -= minutes * 60;
-    seconds = t % 60;
-
-    return [
-      days + 'd',
-      hours + 'h',
-      minutes + 'm',
-      seconds + 's'
-    ].join(' ');
-  }
-
 
   ngOnInit() {
     this.participantList = this.participantService.getParticipants();
     //this.future = new Date(this.futureString);
     this.future = new Date();
-    this.future.setMinutes(this.future.getMinutes() + 2) ;
-    console.log(this.future);
-    this.$counter = Observable.interval(1000).map((x) => {
-      this.diff = Math.floor((this.future.getTime() - new Date().getTime()) / 1000);
-      return x;
-    });
+    //this.future.setMinutes(this.future.getMinutes() + 2) ;
 
-    this.subscription = this.$counter.subscribe((x) => this.message = this.dhms(this.diff));
   }
 
   ngOnDestroy(): void {
+    this.stopTimer();
+  }
+
+  stopTimer() {
     this.subscription.unsubscribe();
   }
 
+  startTimer() {
+    // set the timer to a time in the future, based on "totalTime" seconds
+    this.future.setSeconds(this.future.getSeconds() + this.totalTime);
 
+    //console.log(this.future);
+    this.$counter = Observable.interval(1000).map((x) => {
+      this.diff = Math.floor((this.future.getTime() - new Date().getTime()) / 1000);
+      this.currentElapsed = this.totalTime - this.diff;
+      return x;
+    });
+
+    this.subscription = this.$counter.subscribe((x) => this.currentDiff);
+
+  }
+
+  nextParticipant() {
+    if (this.currentParticipant < this.participantList.length) {
+      this.currentParticipant++;
+    } else {
+      this.message = "No more participants";
+    }
+  }
+
+
+  shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
 
 
 
