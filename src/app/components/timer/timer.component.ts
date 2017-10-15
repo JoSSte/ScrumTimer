95096d7825c39f384deeab7ae39709fa9cec87b9
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2017-10-15 18:18:37 +0200

    cleaned up stoptimer() and starttimer(). Removed debug variables.

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -17,23 +17,21 @@ export class TimerComponent implements OnInit {
 
   // variables for session
   private totalMaxTime = 60 * 15;
+  private totalTime: number = 120;
   private recommendedIndividualTime = 120;
-  private currentParticipant = 0;
   private overallPercent = 0;
 
   // variables for indiviual
   private individualMaxTime = 120;
   private currentPercent: number = 0;
+  private currentParticipant: Participant;
 
   private future: Date;
   private futureString: string;
   private diff: number;
   private currentDiff: number;
   private currentElapsed: number = 0;
-
-  private totalTime: number = 120;
-
-  private message: string;
+  private timerActive:boolean = false;
 
   constructor(public participantService: ParticipantService, elm: ElementRef) {
   }
@@ -43,7 +41,7 @@ export class TimerComponent implements OnInit {
     //this.future = new Date(this.futureString);
     this.future = new Date();
     //this.future.setMinutes(this.future.getMinutes() + 2) ;
-
+    this.currentParticipant = this.participatingParticipants[0];
   }
 
   ngOnDestroy(): void {
@@ -51,7 +49,13 @@ export class TimerComponent implements OnInit {
   }
 
   stopTimer() {
+    //stop timer
     this.subscription.unsubscribe();
+    //set nextParticipant
+    this.nextParticipant();
+    // set progressbar to 100%
+    this.currentPercent = 100;
+    this.timerActive = false;
   }
 
   startTimer() {
@@ -67,13 +71,18 @@ export class TimerComponent implements OnInit {
     });
 
     this.subscription = this.$currentCounter.subscribe((x) => this.currentDiff);
+    this.timerActive = true;
   }
 
   nextParticipant() {
+    //save time for participant
+    this.participatingParticipants[0].time = this.currentElapsed;
+    this.currentElapsed = 0;
     //move first Participant to done participants
     this.doneParticipants.push(this.participatingParticipants[0]);
     //remove the top participant
     this.participatingParticipants.splice(0, 1);
+    this.overallPercent = Math.round((this.doneParticipants.length / (this.participatingParticipants.length + this.doneParticipants.length)) * 100);
   }
 
   shuffle(array) {
