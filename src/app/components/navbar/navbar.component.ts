a0b8d8b6b45f6ee241a8a5184edc51090633b45d
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2021-07-20 00:48:48 +0200

    Added popout functionality If the window.opener is null an extra menu item called PopOut is shown. Clicking this will open ScrumTimer in a 350px window and if localStorage is set accordingly Jira in another window

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,9 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
+  noOpener = true;
+  scrumTimerWindow = null;
+  jiraTimerWindow = null;
+  private windowProps = 'resizable=no,scrollbars,status,top=0,right=0,width=';
+  private popupWidth = 360;
+  constructor() {
+    const hasOpener = window.opener;
+    let openerMessage = '';
+    try {
+      if (hasOpener) {
+        this.noOpener = false;
+        openerMessage = 'Not Displaying popout link. already popped out';
+      } else {
+        this.noOpener = true;
+        openerMessage = 'Displaying pop out link';
+      }
+    } catch (e) {
+      console.log(e);
+      openerMessage = 'Not Displaying popout link. Already popped out (exception)';
+      this.noOpener = false;
+    }
+    console.log(openerMessage);
+  }
+  openScrumTimer() {
+    const swp = this.windowProps+ this.popupWidth + ',height=' + window.screen.availHeight;
+    this.scrumTimerWindow = window.open(window.document.URL, 'TimerWindowName', swp );
+  }
+  openJira() {
+    const jwp = this.windowProps + (window.screen.availWidth - this.popupWidth - 10) + ',height=' + window.screen.availHeight;
+    let url = localStorage.getItem('JiraURL');
+    if (!url) {
+      url = 'about:blank';
+      console.log('no JiraURL in localStorage');
+    }
+    this.jiraTimerWindow = window.open(url, 'StoryWindowName', jwp);
+  }
+  openWindows() {
+    console.log('Opening windows');
+    this.openJira();
+    this.openScrumTimer();
+  }
   ngOnInit() {
   }
 
