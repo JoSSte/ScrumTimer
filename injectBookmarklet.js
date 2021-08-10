b17e7ff1d3b346e06f25b389499dd584eb90ff9b
Jonas Stumph Stevnsvig <jonas.stumph@stevnsvig.com>
2021-08-10 12:46:28 +0200

    Added bookmarklet to open in jira frames

--- injectBookmarklet.js
+++ injectBookmarklet.js
@@ -4,3 +4,12 @@ let d = document.createElement("div");
 d.innerHTML = '<iframe src="http://localhost:4200/#/" style=" width:100%; height:100%"> Hello World </iframe>';
 d.style='position: relative; top: 0; right:0; width:250px; bottom: 0; z-index: 100; background-color: rgba(100,100,100,0.9) ';
 document.getElementsByTagName('body')[0].append(d);
+
+ //Jira specific - inserts into div next to Jira details
+
+let d = document.createElement("div");
+d.innerHTML = '<iframe src="http://localhost:4200/#/" style=" width:100%; height:100%"> Hello World </iframe>';
+d.class="ghx-detail-view gh-editable-detail-view ui-resizable";
+d.style="width:400px";
+document.getElementById('ghx-work').append(d);
+
