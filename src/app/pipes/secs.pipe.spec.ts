1545626e0f37f3a68d44fa01862f2426dbfaf9a3
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2023-12-01 23:17:28 +0100

    Made secspipe tests parameterized

--- src/app/pipes/secs.pipe.spec.ts
+++ src/app/pipes/secs.pipe.spec.ts
@@ -6,16 +6,23 @@ describe('SecsPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('transforms 100 to "01:40"', () => {
-    expect(pipe.transform(100)).toBe('01:40');
-  });
-
-  it('transforms 10 to "00:10"', () => {
-    expect(pipe.transform(10)).toBe('00:10');
-  });
-
-  it('transforms -70 to "-01:10"', () => {
-    expect(pipe.transform(-70)).toBe('-01:10');
-  });
+  for (const {testId, inputValue, expectedOutput} of [
+      { testId: 0, inputValue:   -70, expectedOutput: '-01:10' },
+      { testId: 1, inputValue:    10, expectedOutput:  '00:10' },
+      { testId: 2, inputValue:   240, expectedOutput:  '04:00' },
+      { testId: 3, inputValue:    60, expectedOutput:  '01:00' },
+      { testId: 4, inputValue:     0, expectedOutput:  '00:00' },
+      { testId: 4, inputValue:    -0, expectedOutput:  '00:00' },
+      { testId: 5, inputValue:   120, expectedOutput:  '02:00' },
+      { testId: 6, inputValue:   671, expectedOutput:  '11:11' },
+      { testId: 6, inputValue:  3599, expectedOutput:  '59:59' },
+      { testId: 6, inputValue: -3599, expectedOutput: '-59:59' },
+      { testId: 7, inputValue:   100, expectedOutput:  '01:40' }
+  ]) {
+    it(`Verify seconds to minutes display [${testId}: ${inputValue} → ${expectedOutput}]`, () => {
+      console.log(inputValue + ' → ' + pipe.transform(inputValue));
+      expect(pipe.transform(inputValue)).toBe(expectedOutput);
+    });
+  }
 
 });
