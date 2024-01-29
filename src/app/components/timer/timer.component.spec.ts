7ec57e328f8bde6021d37369a553570fbdb723f5
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2024-01-29 22:04:15 +0100

    Improve code coverage of TimerComponent

--- src/app/components/timer/timer.component.spec.ts
+++ src/app/components/timer/timer.component.spec.ts
@@ -3,6 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { Participant } from '../../models/Participant';
 import { ParticipantService } from '../../services/participant/participant.service';
 import { SettingsService } from '../../services/settings/settings.service';
 import { TimerComponent } from './timer.component';
@@ -29,7 +30,7 @@ describe('TimerComponent', () => {
         SecsPipe
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -38,7 +39,52 @@ describe('TimerComponent', () => {
     fixture.detectChanges();
   });
   // TODO: improve tests https://angular.io/guide/testing-components-basics
-   it('should create', () => {
-     expect(component).toBeTruthy();
-   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should start with no done participants', () => {
+    expect(component.doneParticipants.length).toEqual(0);
+  });
+
+  it('works With Individual Max Time', () => {
+    component.settingsService.setUseGlobalMaxTime(false);
+    expect(component.totalMaxTime).toEqual(component.settingsService.getGlobalMaxTime());
+  });
+
+  it('Has some Participants, but none done, none absent as default', () => {
+    console.log(component.participantList.length);
+    expect(component.participantList.length).toBeGreaterThan(0);
+    expect(component.doneParticipants.length).toEqual(0);
+    expect(component.absentParticipants.length).toEqual(0);
+  });
+  it('Should decrease ParticipantList and increase DoneParticipantList when Next is clicked', () => {
+    const initialList = component.participantList.length;
+    const initialDoneList = component.doneParticipants.length;
+    expect(component.participantList.length).toBeGreaterThan(0);
+    expect(component.doneParticipants.length).toEqual(0);
+    component.startTimer();
+    component.nextParticipant();
+    expect(component.participantList.length).toEqual(initialList - 1);
+    expect(component.doneParticipants.length).toEqual(initialDoneList + 1);
+  });
+  // it('ParticipantList initial length should equal DoneParticipantList when all are through', () => {
+  //   // console.log(component.participantList);
+  //   component.participantList.push(new Participant('JVA', 'Joakim von And'));
+  //   component.participantList.push(new Participant('AA', 'Anders And'));
+  //   component.participantList.push(new Participant('DSY', 'Andersine And'));
+  //   // console.log(component.participantList);
+  // 
+  //   const initialList = component.participantList.length;
+  //   expect(component.participantList.length).toBeGreaterThan(0);
+  //   expect(component.doneParticipants.length).toEqual(0);
+  //   expect(component.absentParticipants.length).toEqual(0);
+  //   component.startTimer();
+  //   for (let participant of component.participantList) {
+  //     console.log(participant.name);
+  //     component.nextParticipant();
+  //   }
+  //   component.stopTimer();
+  //   expect(component.doneParticipants.length).toEqual(initialList);
+  // });
 });
