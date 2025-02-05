ec86ded742c13572d3cae61e0839630bc317448a
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2025-02-05 22:10:40 +0100

    upgrade to Angluar 19

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import packageInfo from '../../../../package.json';
 
 @Component({
-  selector: 'app-help',
-  templateUrl: './help.component.html',
-  styleUrls: ['./help.component.css']
+    selector: 'app-help',
+    templateUrl: './help.component.html',
+    styleUrls: ['./help.component.css'],
+    standalone: false
 })
 export class HelpComponent implements OnInit {
   public version: string = packageInfo.version;
